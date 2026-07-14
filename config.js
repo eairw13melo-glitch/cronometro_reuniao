1ca60fb4cbe17b9a2c3da56365acb9d4e09b2bf9
eairw13melo-glitch <eairw13melo@gmail.com>
2026-07-14 11:13:45 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-/*
-  CONFIGURAÇÃO OPCIONAL DO SUPABASE
-  A chave publishable/anon pode ficar no navegador quando as políticas RLS do schema.sql estiverem habilitadas.
-  Nunca coloque a service_role aqui.
-*/
-window.CRONOMETRO_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: ''
-};

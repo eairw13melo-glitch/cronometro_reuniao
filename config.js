db8a1ab442b1ca50a318225ecb4aa521eec08f67
eairw13melo-glitch <eairw13melo@gmail.com>
2026-07-15 08:38:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
   3. Nunca use a chave service_role neste arquivo.
 */
 window.CRONOMETRO_CONFIG = {
-  supabaseUrl: 'https://xozyhgyxtgcckxrebxpt.supabase.co/rest/v1/',
+  supabaseUrl: 'https://xozyhgyxtgcckxrebxpt.supabase.co',
   supabasePublishableKey: 'sb_publishable_wo-pzlQ_741ESNmKOTFvRg_m9HYSJOG'
 };
